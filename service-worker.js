bd5e3158a699578fc926e7b4751bba565e8f0400
sanplayer <splayer.contact@gmail.com>
2026-04-10 08:49:16 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER - SanPlayer PWA
 // ============================================================================
 
-const CACHE_NAME = 'sanplayer-v1.0.5';
+const CACHE_NAME = 'sanplayer-v1.0.6';
 const URLS_TO_CACHE = [
     '/',
     '/index.html',
