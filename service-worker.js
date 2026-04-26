df8ebbb8f3c9a22b95cae63889b5d244f5762bf0
sanplayer <splayer.contact@gmail.com>
2026-04-25 21:25:56 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER - SanPlayer PWA
 // ============================================================================
 
-const CACHE_NAME = 'sanplayer-v1.4.8';
+const CACHE_NAME = 'sanplayer-v1.4.9';
 const URLS_TO_CACHE = [
     '/',
     '/style.css',
